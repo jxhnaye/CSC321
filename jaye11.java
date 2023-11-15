2599e841472a85d6319d61337ff531d112a63baa
John Aye <jxhnaye@gmail.com>
2023-11-15 01:07:48 +0000

    Lab 11

---
+++ jaye11.java
@@ -0,0 +1,21 @@
+
+//John Aye
+//CSC321
+//Lab 11
+
+import java.util.Scanner;
+
+public class jaye11 {
+        public static void main(String[] args) {
+                Scanner s = new Scanner(System.in);
+                System.out.println("Enter a number: ");
+
+                int num = s.nextInt();
+                double numSq = Math.pow(num, 2);
+
+                System.out.println("Your number squard is: " + numSq);
+                System.out.println("Don't poke the bear.");
+
+        }
+}
+
